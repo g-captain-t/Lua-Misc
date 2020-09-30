b9ff36830f85505ac1690015619199cf45f122b0
g-captain-t <35653836+g-captain-t@users.noreply.github.com>
2020-09-30 15:36:57 +0700

    Major Rewriting, convert to setPrimaryPartCFrame()
    Realized that model:MoveTo() only uses the root part's CFrame.
    I converted the entire script to instead use :setPrimaryPartCFrame by creating a temporary PrimaryPart in the very center of the model, then setting the CFrame using this function. Works with a far polished reliability.
    
    had a horrible time with CFrames

--- Align Tool Temporary.lua
+++ Align Tool Temporary.lua
@@ -1,6 +1,5 @@
 --Roblox took down Align Tool. This is the script I use to align two parts. Select two parts, modify Axis and paste in console.
-
-local Axis = "Y"
+local Axis = "X"
 
 local Selection = game:GetService("Selection")
 local ChangeHistoryService = game:GetService("ChangeHistoryService")
@@ -9,78 +8,53 @@ local currentSelections = Selection:Get()
 local sOne = currentSelections[1]
 local sTwo = currentSelections[2]
 
+
 -- Functions
-function GetBoundingBox(model, orientation)
-	if typeof(model) == "Instance" then
-		model = model:GetDescendants()
-	end
-	if not orientation then
-		orientation = CFrame.new()
-	end
-	local abs = math.abs
-	local inf = math.huge
-	
-	local minx, miny, minz = inf, inf, inf
-	local maxx, maxy, maxz = -inf, -inf, -inf
-	
-	for _, obj in pairs(model) do
-		if obj:IsA("BasePart") then
-			local cf = obj.CFrame
-			cf = orientation:toObjectSpace(cf)
-			local size = obj.Size
-			local sx, sy, sz = size.X, size.Y, size.Z
-			
-			local x, y, z, R00, R01, R02, R10, R11, R12, R20, R21, R22 = cf:components()
-			
-			local wsx = 0.5 * (abs(R00) * sx + abs(R01) * sy + abs(R02) * sz)
-			local wsy = 0.5 * (abs(R10) * sx + abs(R11) * sy + abs(R12) * sz)
-			local wsz = 0.5 * (abs(R20) * sx + abs(R21) * sy + abs(R22) * sz)
-			
-			if minx > x - wsx then
-				minx = x - wsx
-			end
-			if miny > y - wsy then
-				miny = y - wsy
-			end
-			if minz > z - wsz then
-				minz = z - wsz
-			end
-			
-			if maxx < x + wsx then
-				maxx = x + wsx
-			end
-			if maxy < y + wsy then
-				maxy = y + wsy
-			end
-			if maxz < z + wsz then
-				maxz = z + wsz
-			end
-		end
-	end
-	
-	local omin, omax = Vector3.new(minx, miny, minz), Vector3.new(maxx, maxy, maxz)
-	local omiddle = (omax+omin)/2
-	local wCf = orientation - orientation.p + orientation:pointToWorldSpace(omiddle)
-	local size = (omax-omin)
-	return wCf, size
-end -- From Devforum post by XAXA. https://devforum.roblox.com/t/216581/8
-function classifyCFrame(instance)
-	if instance:IsA("Model") then return instance:GetModelCFrame() 
-	elseif instance:IsA("BasePart") then local CF, size = GetBoundingBox (instance)return CF end
+-- Generates a temporary primary part in the very center
+function getModelCFrame(model)
+	local tempPrimaryPart = Instance.new("Part", workspace)
+	tempPrimaryPart.CFrame = model:GetBoundingBox()
+	local modelCFrame = tempPrimaryPart.CFrame
+	tempPrimaryPart:Destroy()
+	return modelCFrame
 end
 
--- Actual thing
+function moveModelCFrame(model,CF) -- Creates primary part 
+	local primaryPart = nil; local tempPrimaryPart
+	if model.PrimaryPart then  primaryPart = model.PrimaryPart end
+	
+	local tempPrimaryPart = Instance.new("Part", workspace)
+	tempPrimaryPart.CFrame = model:GetBoundingBox()
+	local modelCFrame = tempPrimaryPart.CFrame
+	tempPrimaryPart.Parent = model
+	model.PrimaryPart = tempPrimaryPart 
+	model:SetPrimaryPartCFrame(CF)
+	
+	if primaryPart then model.PrimaryPart = primaryPart end
+	tempPrimaryPart:Destroy()
+end
+
+function classifyCFrame(instance)
+	if instance:IsA("Model") then return getModelCFrame(instance)
+	elseif instance:IsA("BasePart") then return instance.CFrame end
+end
+
+
+
+
+-- Actual thing. One: moving object, Two: target object
 local sOneCFrame = classifyCFrame(sOne)
 local sTwoCFrame = classifyCFrame(sTwo)
-local posX, posY, posZ
 
-if Axis == "Y" then posX, posY, posZ = sOneCFrame.X, sTwoCFrame[Axis], sOneCFrame.Z 
-elseif Axis == "X" then posX, posY, posZ = sTwoCFrame[Axis], sOneCFrame.Y, sOneCFrame.Z 
-elseif Axis == "Z" then posX, posY, posZ = sOneCFrame.X, sOneCFrame.Y, sTwoCFrame[Axis] end
+	-- Generating new CFrame, updating current with the new axis value
+local x, y, z, m11, m12, m13, m21, m22, m23, m31, m32, m33 = sOneCFrame:components()
+print("SONE",x, y, z, m11, m12, m13, m21, m22, m23, m31, m32, m33)
+if Axis == "Y" then y = sTwoCFrame[Axis]
+elseif Axis == "X" then x = sTwoCFrame[Axis]
+elseif Axis == "Z" then z = sTwoCFrame[Axis] end
+local newCFrame = CFrame.new(x, y, z, m11, m12, m13, m21, m22, m23, m31, m32, m33)
 
-sOne:MoveTo(Vector3.new(posX, posY, posZ))
-
-print(sTwoCFrame[Axis], sOneCFrame[Axis])
+moveModelCFrame(sOne,newCFrame)
 
 local logMessage = "[Align] "..sOne.Name.." moved to "..sTwo.Name.." Axis "..Axis
 ChangeHistoryService:SetWaypoint(logMessage) print(logMessage)
