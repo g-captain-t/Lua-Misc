54a23ff2806a5da78e03d11af7c43605e655726b
g-captain-t <35653836+g-captain-t@users.noreply.github.com>
2020-09-30 12:13:01 +0700

    Output logging changed to CFrame[Axis]

--- Align Tool Temporary.lua
+++ Align Tool Temporary.lua
@@ -10,11 +10,16 @@ local sOne = currentSelections[1]
 local sTwo = currentSelections[2]
 
 -- Functions
-function GetBoundingBox(model, orientation) -- From Devforum post by XAXA. https://devforum.roblox.com/t/216581/8. Alternative to Model:GetCFrame()
-	if typeof(model) == "Instance" then model = model:GetDescendants() end
-	if not orientation then orientation = CFrame.new() end
+function GetBoundingBox(model, orientation)
+	if typeof(model) == "Instance" then
+		model = model:GetDescendants()
+	end
+	if not orientation then
+		orientation = CFrame.new()
+	end
 	local abs = math.abs
 	local inf = math.huge
+	
 	local minx, miny, minz = inf, inf, inf
 	local maxx, maxy, maxz = -inf, -inf, -inf
 	
@@ -24,16 +29,32 @@ function GetBoundingBox(model, orientation) -- From Devforum post by XAXA. https
 			cf = orientation:toObjectSpace(cf)
 			local size = obj.Size
 			local sx, sy, sz = size.X, size.Y, size.Z
+			
 			local x, y, z, R00, R01, R02, R10, R11, R12, R20, R21, R22 = cf:components()
+			
 			local wsx = 0.5 * (abs(R00) * sx + abs(R01) * sy + abs(R02) * sz)
 			local wsy = 0.5 * (abs(R10) * sx + abs(R11) * sy + abs(R12) * sz)
 			local wsz = 0.5 * (abs(R20) * sx + abs(R21) * sy + abs(R22) * sz)
-			if minx > x - wsx then minx = x - wsx end
-			if miny > y - wsy then miny = y - wsy end
-			if minz > z - wsz then minz = z - wsz end
-			if maxx < x + wsx then maxx = x + wsx end
-			if maxy < y + wsy then maxy = y + wsy end
-			if maxz < z + wsz then maxz = z + wsz end
+			
+			if minx > x - wsx then
+				minx = x - wsx
+			end
+			if miny > y - wsy then
+				miny = y - wsy
+			end
+			if minz > z - wsz then
+				minz = z - wsz
+			end
+			
+			if maxx < x + wsx then
+				maxx = x + wsx
+			end
+			if maxy < y + wsy then
+				maxy = y + wsy
+			end
+			if maxz < z + wsz then
+				maxz = z + wsz
+			end
 		end
 	end
 	
@@ -42,13 +63,13 @@ function GetBoundingBox(model, orientation) -- From Devforum post by XAXA. https
 	local wCf = orientation - orientation.p + orientation:pointToWorldSpace(omiddle)
 	local size = (omax-omin)
 	return wCf, size
-end 
+end -- From Devforum post by XAXA. https://devforum.roblox.com/t/216581/8
 function classifyCFrame(instance)
 	if instance:IsA("Model") then return instance:GetModelCFrame() 
 	elseif instance:IsA("BasePart") then local CF, size = GetBoundingBox (instance)return CF end
 end
 
--- Continue
+-- Actual thing
 local sOneCFrame = classifyCFrame(sOne)
 local sTwoCFrame = classifyCFrame(sTwo)
 local posX, posY, posZ
@@ -57,9 +78,9 @@ if Axis == "Y" then posX, posY, posZ = sOneCFrame.X, sTwoCFrame[Axis], sOneCFram
 elseif Axis == "X" then posX, posY, posZ = sTwoCFrame[Axis], sOneCFrame.Y, sOneCFrame.Z 
 elseif Axis == "Z" then posX, posY, posZ = sOneCFrame.X, sOneCFrame.Y, sTwoCFrame[Axis] end
 
-print(sTwoCFrame)print(posX, posY, posZ)
-
 sOne:MoveTo(Vector3.new(posX, posY, posZ))
+
+print(sTwoCFrame[Axis], sOneCFrame[Axis])
 
 local logMessage = "[Align] "..sOne.Name.." moved to "..sTwo.Name.." Axis "..Axis
 ChangeHistoryService:SetWaypoint(logMessage) print(logMessage)
