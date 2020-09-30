c82c722e79c41eb109ed65d23c4ca4addc6ed1ad
g-captain-t <35653836+g-captain-t@users.noreply.github.com>
2020-09-30 15:56:58 +0700

    Support for multiple parts with movingSelections array
    sOne (selectionOne) is the current moving part, sTwo (selectionTwo) is the stationary target object.

--- Align Tool Temporary.lua
+++ Align Tool Temporary.lua
@@ -1,16 +1,20 @@
---Roblox took down Align Tool. This is the script I use to align two parts. Select two parts, modify Axis and paste in console.
+--Roblox took down Align Tool. This is the script I use to align multiple parts. 
+--Select the moving parts first and the target object last, modify Axis and paste in console.
 local Axis = "X"
 
 local Selection = game:GetService("Selection")
 local ChangeHistoryService = game:GetService("ChangeHistoryService")
 local currentSelections = Selection:Get()
 
-local sOne = currentSelections[1]
-local sTwo = currentSelections[2]
-
+local sTwo = currentSelections[#currentSelections]
+local movingSelections = {}
+for i=1, #currentSelections do
+	if i < #currentSelections then print(currentSelections[i].Name)
+	movingSelections[#movingSelections+1] = currentSelections[i] end
+end
 
 -- Functions
--- Generates a temporary primary part in the very center
+
 function getModelCFrame(model)
 	local tempPrimaryPart = Instance.new("Part", workspace)
 	tempPrimaryPart.CFrame = model:GetBoundingBox()
@@ -39,22 +43,26 @@ function classifyCFrame(instance)
 	elseif instance:IsA("BasePart") then return instance.CFrame end
 end
 
-
+function classifyMove (instance, CF)
+	if instance:IsA("Model") then moveModelCFrame(instance,CF)
+	elseif instance:IsA("BasePart") then instance.CFrame = CF end
+end
 
 
 -- Actual thing. One: moving object, Two: target object
-local sOneCFrame = classifyCFrame(sOne)
 local sTwoCFrame = classifyCFrame(sTwo)
+for i, sOne in pairs(movingSelections) do
+	local sOneCFrame = classifyCFrame(sOne)
 
-	-- Generating new CFrame, updating current with the new axis value
-local x, y, z, m11, m12, m13, m21, m22, m23, m31, m32, m33 = sOneCFrame:components()
-print("SONE",x, y, z, m11, m12, m13, m21, m22, m23, m31, m32, m33)
-if Axis == "Y" then y = sTwoCFrame[Axis]
-elseif Axis == "X" then x = sTwoCFrame[Axis]
-elseif Axis == "Z" then z = sTwoCFrame[Axis] end
-local newCFrame = CFrame.new(x, y, z, m11, m12, m13, m21, m22, m23, m31, m32, m33)
+	local x, y, z, m11, m12, m13, m21, m22, m23, m31, m32, m33 = sOneCFrame:components()
+	if Axis == "Y" then y = sTwoCFrame[Axis]
+	elseif Axis == "X" then x = sTwoCFrame[Axis]
+	elseif Axis == "Z" then z = sTwoCFrame[Axis] end
+	local newCFrame = CFrame.new(x, y, z, m11, m12, m13, m21, m22, m23, m31, m32, m33)
 
-moveModelCFrame(sOne,newCFrame)
+	classifyMove(sOne,newCFrame)
 
-local logMessage = "[Align] "..sOne.Name.." moved to "..sTwo.Name.." Axis "..Axis
-ChangeHistoryService:SetWaypoint(logMessage) print(logMessage)
+	local logMessage = "[Align] "..sOne.Name.." moved to "..sTwo.Name.." Axis "..Axis
+	ChangeHistoryService:SetWaypoint(logMessage) print(logMessage)
+	
+end
