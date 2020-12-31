42c01f4608c4a93e6258905294f329f164a22478
g-captain-t <35653836+g-captain-t@users.noreply.github.com>
2020-12-31 22:35:43 +0700

    Update Swordfighters.lua

--- neuralnets/jobro13Neuro/Swordfighters.lua
+++ neuralnets/jobro13Neuro/Swordfighters.lua
@@ -1,24 +1,23 @@
 --[[
-Swordfighters Neural Network Experiment
-> The fighters only travelled on the positive axis. Not sure why, but this is the code I played with.
+Swordfighters
+- The bots know their position, their health, their enemy's position, and the enemy's health.
+- The outputs are their next direction,their rotation, and previously, whether to swing their weapons
+- Two bots are put up against each other at a time. Only the winner has a score based on their win time.
 ]]
-
 local neuro = require(script.Parent.Neuro)
 hint = Instance.new("Hint", script.Parent)
 population = neuro.new("population")
 
-for i = 1 , 2 do
-	population:AddBrain(neuro.new("network",2,6,2,8))
+for i = 1 , 20 do
+	population:AddBrain(neuro.new("network",6,4,2,8))
 end
 
 local input_Record = {}
 local output_Record = {}
 
 
-
-
 for generation = 1, math.huge do
-	
+	print("Generation"..generation)
 	for i=1, #population.Brains, 2 do
 		local brain1 = population.Brains[i]
 		local brain2 = population.Brains[i+1]
@@ -39,46 +38,45 @@ for generation = 1, math.huge do
 
 		while not skip do game:GetService("RunService").Heartbeat:Wait()
 			local inputs1 = {
-				--dummy1.Torso.Position.X,
-				--dummy1.Torso.Position.Z,
-				--dummy1.HealthVal.Value,
-				dummy2.Torso.Position.X,
-				dummy2.Torso.Position.Z,
-				--dummy2.HealthVal.Value
-			}
-			outputs1 = brain1:evaluate(inputs1)
-			dummy1.HumanoidRootPart.CFrame = dummy1.HumanoidRootPart.CFrame * CFrame.Angles(0,outputs1[4]/5,0)
-			dummy1.Humanoid:MoveTo(dummy1.Torso.Position + dummy1.Torso.CFrame.LookVector*5*outputs1[5]
-				--[[Vector3.new(
-				outputs1[1]*(outputs1[5]>0.5 and -1 or 1), 
-				dummy2.Torso.Position.Y,
-				outputs1[2]*(outputs1[6]>0.5 and -1 or 1)
-				)]])
-			if outputs1[3] > 0.2 then dummy1.Sword:Activate() end
-			
-			local inputs2 = {
-				--dummy2.Torso.Position.X,
-				--dummy2.Torso.Position.Z,
-				--dummy2.HealthVal.Value,
 				dummy1.Torso.Position.X,
 				dummy1.Torso.Position.Z,
-				--dummy1.HealthVal.Value
+				dummy1.HealthVal.Value,
+				dummy2.Torso.Position.X,
+				dummy2.Torso.Position.Z,
+				dummy2.HealthVal.Value
+			}
+			outputs1 = brain1:evaluate(inputs1)
+			dummy1.HumanoidRootPart.CFrame = dummy1.HumanoidRootPart.CFrame * CFrame.Angles(0,outputs1[4]-0.5/5,0)
+			dummy1.Humanoid:MoveTo(Vector3.new(
+				dummy2.Torso.Position.X+ outputs1[1]-0.5, 
+				dummy2.Torso.Position.Y,
+				dummy2.Torso.Position.Z+ outputs1[2]-0.5
+				))
+			if outputs1[3] > 0 then dummy1.Sword:Activate() end
+			
+			local inputs2 = {
+				dummy2.Torso.Position.X,
+				dummy2.Torso.Position.Z,
+				dummy2.HealthVal.Value,
+				dummy1.Torso.Position.X,
+				dummy1.Torso.Position.Z,
+				dummy1.HealthVal.Value
 			}
 			outputs2 = brain2:evaluate(inputs2)
-			dummy2.HumanoidRootPart.CFrame = dummy2.HumanoidRootPart.CFrame * CFrame.Angles(0,outputs2[4]/5,0)
-			dummy2.Humanoid:MoveTo(dummy2.Torso.Position + dummy2.Torso.CFrame.LookVector*5*outputs2[5]
-			--[[Vector3.new(
-				outputs2[1]*(outputs2[5]>0.2 and -1 or 1), 
+			dummy2.HumanoidRootPart.CFrame = dummy2.HumanoidRootPart.CFrame * CFrame.Angles(0,outputs2[4]-0.5/5,0)
+			dummy2.Humanoid:MoveTo(Vector3.new(
+				dummy2.Torso.Position.X + outputs2[1]-0.5, 
 				dummy2.Torso.Position.Y,
-				outputs2[2]*(outputs2[6]>0.2 and -1 or 1)
-				))]]
-				)
-			if outputs2[3] > 0.2 then dummy2.Sword:Activate() end
+				dummy2.Torso.Position.Z + outputs2[2]-0.5
+				))
+			if outputs2[3] > 0 then dummy2.Sword:Activate() end
 			if dummy1.HealthVal.Value <=0 then
 				winnerbrain = brain2
+				print("brain"..(i+1).."won")
 				skip = true
 			elseif dummy2.HealthVal.Value <=0 then
 				winnerbrain = brain1
+				print("brain"..(i).."won")
 				skip = true
 			end
 		end
