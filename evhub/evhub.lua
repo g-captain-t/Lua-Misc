c0c9a861cd5d1985dd7b87436378e387e5b1fa2a
g-captain-t <35653836+g-captain-t@users.noreply.github.com>
2021-01-04 20:26:02 +0700

    hub.FireClient

--- evhub/evhub.lua
+++ evhub/evhub.lua
@@ -1,3 +1,34 @@
+--[[
+EVHUB
+Inspired by PubSub, evhub completely removes the need to manually place remote and 
+bindable events/functions. Client and server communication can be easily accessed 
+only by passing on a name.
+
+Client:
+	local evhub = require(game.ReplicatedStorage.evhub)
+	evhub.Fire("MessageEvent", "Hello World!")
+
+Server:
+	local evhub = require(game.ReplicatedStorage.evhub)
+	evhub.Listen("MessageEvent", function(player, message)
+		print(player.Name.." says "..message)
+	end)
+
+FUNCTIONS
+"Env" refers to Bindables, which work in the same environment.
+- hub.Fire(string name, tuple parameters)
+- hub.Listen(string name, function callback)
+- hub.FireEnv(string name, tuple parameters)
+- hub.ListenEnv(string name, function callback)
+- hub.FireClient(string name, instance player, tuple parameters)
+
+- hub.Invoke(string name, tuple parameters)
+- hub.Respond(string name, function)
+- hub.InvokeEnv(string name, tuple parameters)
+- hub.RespondEnv(string name, function)
+
+]]
+
 local hub = {}
 
 local function context()
@@ -5,7 +36,9 @@ local function context()
 	return RS:IsServer() and "Server" or RS:IsClient() and "Client"
 end
 
+
 --// EVENTS	(Fire and Listen)
+
 local RemoteEvents = script.RemoteEvents
 local EnvEvents = script.EnvEvents
 
@@ -43,6 +76,15 @@ function hub.Fire(name, ...)
 	end
 end
 
+function hub.FireClient(name, client, ...)
+	local context = context()
+	local connection
+	local ev = findEvent(RemoteEvents, name, context)
+	if context=="Server" then
+		ev:FireClient(client, ...)
+	end
+end
+
 function hub.Listen (name, callback)
 	local context = context()
 local ev = findEvent(RemoteEvents, name,context)
@@ -65,6 +107,7 @@ end
 
 
 --// FUNCTIONS (Invoke and Respond)
+
 local RemoteFns= script.RemoteFunctions
 local EnvFns = script.EnvFunctions
 
