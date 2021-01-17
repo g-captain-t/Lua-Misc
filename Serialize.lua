bad2cc4741ece92b92a0035523833be849c61f13
g_captain <35653836+g-captain-t@users.noreply.github.com>
2021-01-17 19:25:58 +0700

    Brickcolor

--- Serialize.lua
+++ Serialize.lua
@@ -1,9 +1,11 @@
 --[[
 
-Serialize by g_captain
+Serialize
+Author: g_captain
 Serializes Roblox datatypes into tables. A serialized value looks like this:
 {"Vector3",{13,4,3}}
 
+BrickColor
 CFrame
 Color3
 EnumItem
@@ -19,118 +21,127 @@ Serialize.SerializeAll(tuple values)
 Serialize.DeserializeAll(tuple values)
 
 ]]
+
 local Serialize = {}
 
 function Serialize.pack(dtype, value)
-    return {dtype, value}
+	return {dtype, value}
 end
 local pack = Serialize.pack
 
 Serialize.lib = {
-    ["CFrame"] = {
-        serialize = function(value)
-            return pack("CFrame", value:GetComponents())
-        end,
-        deserialize = function(value)
-            return CFrame.new(table.unpack(value))
-        end
-    },
-    ["Color3"] = {
-        serialize = function(value)
-            return pack("Color3", {value:toRGB()})
-        end,
-        deserialize = function(value)
-            return Color3.fromRGB(table.unpack(value))
-        end
-    },
-    ["EnumItem"] = {
-        serialize = function(value)
-            local str = tostring(value)
-            local _, _, k, v = str:find("Enum%.(%a+)%.(%a+)")
-            local split = {k, v}
-            return pack("EnumItem", split)
-        end,
-        deserialize = function(value)
-            return Enum[value[1]][value[2]]
-        end
-    },
-    ["NumberRange"] = {
-        serialize = function(value)
-            return pack("NumberRange", {value.Min, value.Max})
-        end,
-        deserialize = function(value)
-            return NumberRange.new(table.unpack(value))
-        end
-    },
-    ["UDim"] = {
-        serialize = function(value)
-            return pack("UDim", {value.X, value.Y})
-        end,
-        deserialize = function(value)
-            return UDim.new(table.unpack(value))
-        end
-    },
-    ["UDim2"] = {
-        serialize = function(value)
-            return pack("UDim2", {value.Scale.X, value.Offset.X, value.Scale.Y, value.Offset.Y})
-        end,
-        deserialize = function(value)
-            return UDim2.new(table.unpack(value))
-        end
-    },
-    ["Vector2"] = {
-        serialize = function(value)
-            return pack("Vector2", {value.X, value.Y})
-        end,
-        deserialize = function(value)
-            return Vector2.new(table.unpack(value))
-        end
-    },
-    ["Vector3"] = {
-        serialize = function(value)
-            return pack("Vector3", {value.X, value.Y, value.Z})
-        end,
-        deserialize = function(value)
-            return Vector3.new(table.unpack(value))
-        end
-    }
+	["BrickColor"] = {
+		serialize = function(value) 
+			return pack("BrickColor", {tostring(value)})
+		end,
+		deserialize = function(value)
+			return BrickColor.new(value)
+		end
+	},
+	["CFrame"] = {
+		serialize = function(value)
+			return pack("CFrame", {value:GetComponents()})
+		end,
+		deserialize = function(value)
+			return CFrame.new(table.unpack(value))
+		end
+	},
+	["Color3"] = {
+		serialize = function(value)
+			return pack("Color3", {value.R*255, value.G*255, value.B*255})
+		end,
+		deserialize = function(value)
+			return Color3.fromRGB(table.unpack(value))
+		end
+	},
+	["EnumItem"] = {
+		serialize = function(value)
+			local str = tostring(value)
+			local _, _, k, v = str:find("Enum%.(%a+)%.(%a+)")
+			local split = {k, v}
+			return pack("EnumItem", split)
+		end,
+		deserialize = function(value)
+			return Enum[value[1]][value[2]]
+		end
+	},
+	["NumberRange"] = {
+		serialize = function(value)
+			return pack("NumberRange", {value.Min, value.Max})
+		end,
+		deserialize = function(value)
+			return NumberRange.new(table.unpack(value))
+		end
+	},
+	["UDim"] = {
+		serialize = function(value)
+			return pack("UDim", {value.X, value.Y})
+		end,
+		deserialize = function(value)
+			return UDim.new(table.unpack(value))
+		end
+	},
+	["UDim2"] = {
+		serialize = function(value)
+			return pack("UDim2", {value.Scale.X, value.Offset.X, value.Scale.Y, value.Offset.Y})
+		end,
+		deserialize = function(value)
+			return UDim2.new(table.unpack(value))
+		end
+	},
+	["Vector2"] = {
+		serialize = function(value)
+			return pack("Vector2", {value.X, value.Y})
+		end,
+		deserialize = function(value)
+			return Vector2.new(table.unpack(value))
+		end
+	},
+	["Vector3"] = {
+		serialize = function(value)
+			return pack("Vector3", {value.X, value.Y, value.Z})
+		end,
+		deserialize = function(value)
+			return Vector3.new(table.unpack(value))
+		end
+	}
 }
 
 function Serialize.Serialize(value)
-    local Type = typeof(value)
-    if Serialize.lib[Type] then
-        return Serialize.lib[Type].serialize(value)
-    else
-        return value
-    end
+	local Type = typeof(value)
+	if Serialize.lib[Type] then
+		return Serialize.lib[Type].serialize(value)
+	else
+		return value
+	end
 end
 
 function Serialize.Deserialize(packed)
-    local Type = typeof(packed)
-    Type = Type=="table" and packed[1] or Type
-    if Serialize.lib[Type] then
-        return Serialize.lib[Type].deserialize(packed[2])
-    else
-        return packed
-    end
+	local Type = typeof(packed)
+	Type = Type=="table" and packed[1] or Type
+	if Serialize.lib[Type] then
+		return Serialize.lib[Type].deserialize(packed[2])
+	else
+		return packed
+	end
 end
 
 function Serialize.SerializeAll(...)
-    local toSerialize = {...}
-    local serialized = {}
-    for _, value in pairs(toSerialize) do
-        table.insert(serialized, Serialize.Serialize(value))
-    end
-    return table.unpack(serialized)
+	local toSerialize = {...}
+	local serialized = {}
+	for _, value in pairs(toSerialize) do
+		table.insert(serialized, Serialize.Serialize(value))
+	end
+	return table.unpack(serialized)
 end
 
 function Serialize.DeserializeAll(...)
-    local toDeserialize = {...}
-    local deserialized = {}
-    for _, packed in pairs(toSerialize) do
-        table.insert(deserialized, Serialize.Deserialize(packed))
-    end
-    return table.unpack(deserialized)
+	local toDeserialize = {...}
+	local deserialized = {}
+	for _, packed in pairs(toDeserialize) do
+		table.insert(deserialized, Serialize.Deserialize(packed))
+	end
+	return table.unpack(deserialized)
 end
 
 return Serialize
