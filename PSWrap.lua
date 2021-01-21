7c87ba9861da707b2f5bc4a87399d28c79a9ed51
g_captain <35653836+g-captain-t@users.noreply.github.com>
2021-01-21 17:58:07 +0700

    Update PSWrap.lua

--- PSWrap.lua
+++ PSWrap.lua
@@ -1,8 +1,6 @@
 --[[
-
 PSWrap
 A bridge between my current data interface (DataCache) and loleris' ProfileService
-
 ]]
 
 local PSWrap = {}
@@ -13,29 +11,62 @@ local Cache = {}
 
 local function rwait() RS.Heartbeat:Wait() end
 
+local function deepcompare(t1,t2,ignore_mt)
+	--// Thanks stackoverflow https://stackoverflow.com/questions/20325332/
+	local ty1 = type(t1)
+	local ty2 = type(t2)
+	if ty1 ~= ty2 then return false end
+	-- non-table types can be directly compared
+	if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
+	-- as well as tables which have the metamethod __eq
+	local mt = getmetatable(t1)
+	if not ignore_mt and mt and mt.__eq then return t1 == t2 end
+	for k1,v1 in pairs(t1) do
+		local v2 = t2[k1]
+		if v2 == nil or not deepcompare(v1,v2) then return false end
+	end
+	for k2,v2 in pairs(t2) do
+		local v1 = t1[k2]
+		if v1 == nil or not deepcompare(v1,v2) then return false end
+	end
+	return true
+end
+
 function PSWrap.Get(key, storename, default)
 	default = default or {}
+	key = tostring(key)
 	local store = ProfileService.GetProfileStore(storename, default)
-	--// Cache the store
+	-- require(game.Selection:Get()[1]).GetProfileStore("MainStore", {}):WipeProfileAsync("161502352")
+
+	--// check the cache
 	Cache[storename] = Cache[storename] or {}
-	profile = Cache[storename][key] or store:LoadProfileAsync(key,"ForceLoad")
-	if profile then 
+	local profile = Cache[storename][key]
+	local iscached = profile~=nil
+	profile = profile or store:LoadProfileAsync(key,"ForceLoad")
+	
+	if profile and not iscached then 
+		--// It's not loaded yet
+		local issame = deepcompare(profile.Data,default,true)
+		if issame or not profile.Data then 
+			--// It's a new profile
+			--// Check if a datastore entry of the same name already exists, load from it
+			local dstore = DS:GetDataStore(storename)
+			local s, data = pcall(function()return dstore:GetAsync(key)end)
+			if s and data then 
+				profile.Data = data
+			end
+		end
 		--// Profile found, fill in missing
 		profile:Reconcile()
-	else 
-		--// Check if a datastore of the same name exists, and if it does, migrate
-		local dstore = DS:GetDataStore(storename)
-		local s, data = pcall(function()return dstore:GetAsync(key)end)
-		if s and data then 
-			profile.Data = data
-		end
 	end
+	
 	--// Cache it if it doesn't exist
 	Cache[storename][key] = Cache[storename][key] or profile
-	return profile.Data, profile
+	return profile and profile.Data, profile
 end
 
 function PSWrap.Save(key, storename, newvalue)
+	key = tostring(key)
 	Cache[storename] = Cache[storename] or {}
 	local profile = Cache[storename][key]
 	if not profile then warn("Profile doesn't exist yet!") return end 
@@ -46,10 +77,11 @@ function PSWrap.Save(key, storename, newvalue)
 	else 
 		profile.Data = newvalue
 	end
-	return profile.Data, profile
+	return profile and profile.Data, profile
 end
 
 function PSWrap.Clear(key, storename)
+	key = tostring(key)
 	Cache[storename] = Cache[storename] or {}
 	local profile = Cache[storename][key]
 	if not profile then warn("Profile doesn't exist yet!") return end 
@@ -57,13 +89,8 @@ function PSWrap.Clear(key, storename)
 	return profile
 end
 
-
-function PSWrap.SaveStore(key, storename)
-	local profile = PSWrap.Clear(key, storename)
-	if profile then profile:Release() end
-end
-
 function PSWrap.UpdateIndex(key, storename, index, newvalue)
+	key = tostring(key)
 	Cache[storename] = Cache[storename] or {}
 	local profile = Cache[storename][key]
 	if not profile then warn("Profile doesn't exist yet!") return end 
@@ -74,7 +101,22 @@ function PSWrap.UpdateIndex(key, storename, index, newvalue)
 	else
 		profile.Data[index] = newvalue
 	end
-	return profile.Data, profile
+	return profile and profile.Data, profile
+end
+
+function PSWrap.SaveStore(key, storename)
+	key = tostring(key)
+	local profile = PSWrap.Clear(key, storename)
+	if profile then profile:Release() end
+end
+
+function PSWrap.ClearStore(key, storename)
+	--// Removes a key completely
+	key = tostring(key)
+	local profile = PSWrap.Clear(key, storename)
+	local pstore = ProfileService.GetProfileStore(storename)
+	pstore:WipeProfileAsync(key)
+	return profile
 end
 
 
@@ -82,6 +124,7 @@ end
 
 function PSWrap.ProfileToDataStore(key, storename)
 	--// Transfers a profile back to DataStore
+	key = tostring(key)
 	local pstore = ProfileService.GetProfileStore(storename, {})
 	local profile = pstore:LoadProfileAsync(key,"ForceLoad")
 	local dstore = DS:GetDataStore(storename)
@@ -100,6 +143,7 @@ end
 function PSWrap.DataStoreToProfile(key, storename)
 	--// loads and sets (which .Get already does), then immediately release
 	--// Meant to NOT be used in-game, as .Get already looks up saved keys
+	key = tostring(key)
 	local _, profile = PSWrap.Get(key, storename, {})
 	profile:Release()
 	warn("Finished migrating data key "..storename.."/"..key.." to ProfileStore!")
