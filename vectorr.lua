a8ed7a41cd59f1d1bf17f116c28a90cbca0b5c7b
g_captain <35653836+gcaptn@users.noreply.github.com>
2021-02-07 18:42:57 +0700

    Create vectorr.lua

---
+++ vectorr.lua
@@ -0,0 +1,43 @@
+--[[
+
+  18:34:44.943  DataModel Loading https://assetdelivery.roblox.com/v1/asset/?id=95206881  -  Studio
+  18:35:20.681  > print(Vector3.new(1,2,1)-1)  -  Studio
+  18:35:20.689  print(Vector3.new(1,2,1)-1):1: invalid argument #2 (Vector3 expected, got number)  -  Edit
+  18:35:20.689  Stack Begin  -  Studio
+  18:35:20.689  Script 'print(Vector3.new(1,2,1)-1)', Line 1  -  Studio
+  18:35:20.689  Stack End  -  Studio
+  18:35:30.721  > print(Vector3.new(1,2,1)/2)  -  Studio
+  18:35:30.721  0.5, 1, 0.5  -  Edit
+  18:35:50.228  > print(Vector3.new(1,2,1)*Vector3.new(2,4,2))  -  Studio
+  18:35:50.228  2, 8, 2  -  Edit
+  18:36:04.505  > print(Vector3.new(1,2,1)/Vector3.new(2,4,2))  -  Studio
+  18:36:04.505  0.5, 0.5, 0.5  -  Edit
+  18:36:22.136  > print(Vector3.new(1,2,1)^Vector3.new(2,4,2))  -  Studio
+  18:36:22.138  print(Vector3.new(1,2,1)^Vector3.new(2,4,2)):1: attempt to perform arithmetic (pow) on Vector3  -  Edit
+  18:36:22.138  Stack Begin  -  Studio
+  18:36:22.138  Script 'print(Vector3.new(1,2,1)^Vector3.new(2,4,2))', Line 1  -  Studio
+  18:36:22.138  Stack End  -  Studio
+  18:37:04.825  >  print(Vector3.new(1,2,1).Magnitude  -  Studio
+  18:37:04.836   print(Vector3.new(1,2,1).Magnitude:1: Expected ')' (to close '(' at column 7), got <eof>  -  Studio
+  18:37:18.161  >  print(Vector3.new(1,2,1).Magnitude)  -  Studio
+  18:37:18.168  2.4494898319244  -  Edit
+  18:37:24.865  >  print(Vector3.new(1,2,1).Unit)  -  Studio
+  18:37:24.869  0.408248276, 0.816496551, 0.408248276  -  Edit
+  18:38:29.889  >  print(Vector3.new(1,2,1):Dot(Vector3.new(2,4,2)))  -  Studio
+  18:38:29.892  12  -  Edit
+  18:38:43.741  >  print(Vector3.new(1,2,1):Cross(Vector3.new(2,4,2)))  -  Studio
+  18:38:43.748  0, 0, 0  -  Edit
+  18:38:49.721  >  print(Vector3.new(1,2,1):Cross(Vector3.new(2,5,2)))  -  Studio
+  18:38:49.722  -1, 0, 1  -  Edit
+  18:39:25.439  Baseplate auto-recovery file was created  -  Studio - C:/Users/Administrator/Documents/ROBLOX/AutoSaves
+  18:39:31.641  >  print(Vector3.new(1,2,1):Lerp (Vector3.new(2,4,2)))  -  Studio
+  18:39:31.646  Argument 3 missing or nil  -  Edit
+  18:39:31.647  Stack Begin  -  Studio
+  18:39:31.647  Script ' print(Vector3.new(1,2,1):Lerp (Vector3.new(2,4,2)))', Line 1  -  Studio
+  18:39:31.647  Stack End  -  Studio
+  18:39:40.808  >  print(Vector3.new(1,2,1):Lerp(Vector3.new(2,4,2), 0.5))  -  Studio
+  18:39:40.813  1.5, 3, 1.5  -  Edit
+  18:39:51.970  >  print(Vector3.new(1,2,1):Lerp(Vector3.new(2,4,2), 0.5)- Vector3.new(1,2,1))  -  Studio
+  18:39:51.972  0.5, 1, 0.5  -  Edit
+
+]]
